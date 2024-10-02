f0889a9cf57646aa327f774a03131f58fb551cd4
henriqueregatieri <riq.regatieri@gmail.com>
2024-10-02 00:21:05 -0300

    feat: Add base layout

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Ponha teu item!' style={styles.textInput} />
+        <Button title='Adicionar' />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>Minha lista</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    gap: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'silver',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'silver',
+    width: '70%',
+    padding: 10,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
