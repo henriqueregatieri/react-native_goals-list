d4f24d77e8331435930894f6e82f6acbabc1d9e2
henriqueregatieri <riq.regatieri@gmail.com>
2024-10-03 23:29:09 -0300

    feat: Add image

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { useState } from 'react';
-import { StyleSheet, View, FlatList, Text } from 'react-native';
+import { StyleSheet, View, FlatList, Text, Image } from 'react-native';
 import { GoalItem } from './components/GoalItem';
 import { GoalInput } from './components/GoalInput';
+import imgHappy from './assets/images/happy.jpg';
 
 export default function App() {
   const [goals, setGoals] = useState([]);
+
+  const isLastItem = (index) => index === goals.length - 1;
 
   return (
     <View style={styles.appContainer}>
@@ -20,7 +23,14 @@ export default function App() {
         <FlatList
           data={goals}
           renderItem={({ item, index }) => (
-            <GoalItem item={item} index={index} setGoals={setGoals} />
+            <>
+              <GoalItem item={item} index={index} setGoals={setGoals} />
+              {isLastItem(index) && (
+                <View style={styles.imageContainer}>
+                  <Image source={imgHappy} style={styles.image} />
+                </View>
+              )}
+            </>
           )}
         />
       </View>
@@ -35,5 +45,13 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 7,
+  },
+  imageContainer: {
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
   },
 });
